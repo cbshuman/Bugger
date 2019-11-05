aa7189443855f6719274384054f6cb313c35e83f
Colton Shuman <43967813+cbshuman@users.noreply.github.com>
2019-11-04 19:49:59 -0700

    Few more fixes, working on getting cookies out of dataAccess, and implementing a commmand structure to seperate Handlers from the Database. Trying not to over abstract.

---
+++ src/bugger/command/ValidateCookie.java
@@ -0,0 +1,45 @@
+package bugger.command;
+
+import bugger.dataAccessInterface.DataProxy;
+import bugger.dataModel.Cookie;
+
+public class ValidateCookie implements BuggerCommand<Boolean>
+	{
+	String username;
+	boolean returnValue = false;
+
+	public ValidateCookie(String username)
+		{
+		this.username = username;
+		}
+
+	@Override
+	public void DoCommand()
+		{
+		Cookie[] cookies = DataProxy.GetUserCookies(username);
+
+		for(int i = 0; i < cookies.length; i++)
+			{
+			if(cookies[i].ValidTimeStamp())
+				{
+				returnValue = true;
+				}
+			else
+				{
+				//TODO : Delete old cookies
+				}
+			}
+		}
+
+	@Override
+	public Boolean CommandSuccessful()
+		{
+		return returnValue;
+		}
+
+	@Override
+	public Boolean GetReturnValue()
+		{
+		return CommandSuccessful();
+		}
+	}
